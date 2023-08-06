cac2bccf1ac5f95cbf91f26a4b9515e1c46b423d
thiagosouza10 <thiagodesouza22@gmail.com>
2023-08-06 17:52:45 -0300

    [Implementação] Relatório mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,18 @@ module.exports = defineConfig({
   env: {
     urlBase: 'https://buger-eats.vercel.app/'
   },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportTitle: 'Relatório de testes automatizados - Buger Eats',
+    reportDir: './logs/',
+    embeddedScreenshots: true,
+    autoOpen: true,
+  },
+  video: true,
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
